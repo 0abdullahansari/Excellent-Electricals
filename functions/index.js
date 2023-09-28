cc20db8a9c493bd233d07fe156b7fed88cad69cf
0abdullahansari <chittij7@gmail.com>
2023-09-28 19:37:12 +0530

    up

--- functions/index.js
+++ functions/index.js
@@ -3,41 +3,43 @@ const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const express = require("express");
 const  cors = require("cors");
-const stripe = require("stripe")(
-    'sk_test_51NrCkySHYBUIxPcUqcQ4Yxypee91ujeoxZKFrhoajdbXSUSUbt0yMLJgJCZxWq26Yq04f0Y6zWmMPPCzKE1jbobk00C1FL3hfF'
-)
 const client_id = "TEST10026894c5d75ec89fb9ed1deaf749862001";
 const client_secret = "TEST7f452f358066d1a990743d267cf5b60ad99d15ec";
 const axios = require("axios")
-
+const orderid = require('order-id')('excel');
+const fs = require('fs')
 const app = express();
 
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
-  try{
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
-    payment_method_types: ['card'],
+// app.post("/payments/create", async (request, response) => {
+//   const total = request.query.total;
+//   try{
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: total, // subunits of the currency
+//     currency: "usd",
+//     payment_method_types: ['card'],
 
-  });
+//   });
 
-  // OK - Created
-  response.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });    
-  }catch(error){
-    console.log(error);
-  }
+//   // OK - Created
+//   response.status(201).send({
+//     clientSecret: paymentIntent.client_secret,
+//   });    
+//   }catch(error){
+//     console.log(error);
+//   }
 
-});
+// });
+
+
 app.post("/pretransaction",async (req,res)=>{
-  const baseUrl = "https://sandbox.cashfree.com/pg/orders";
-  const body = req.body;
-  console.log(body)
+  // const baseUrl = "https://sandbox.cashfree.com/pg/orders";
+  // const body = req.body;
+  // console.log(body)
+    console.log(req.body);
+    id=orderid.generate();
     const options = {
       url: "https://sandbox.cashfree.com/pg/orders",
       method: "post",
@@ -49,18 +51,28 @@ app.post("/pretransaction",async (req,res)=>{
         'x-api-version': '2022-09-01'
       },
       data: {
-        order_id: ""+"order_"+Math.ceil(Math.random()*10000),
-        order_amount: 200,
+        order_id: String(id),
+        order_amount: req.body.amount,
         order_currency: "INR",
         customer_details: {
-          customer_id:"blahs23",
-          customer_phone: "9871887737",
+          customer_id: req.body.customerPhone,
+          customer_email: req.body.customerId,
+          customer_phone: req.body.customerPhone,
         },
+        order_meta: {return_url: `http://localhost:5173/orders?order_id=${id}`}
       }
     }
-    const response = await axios(options)
-    console.log(response.data);
-    res.status(200).send(response.data); 
+    try{
+      const response = await axios(options);
+      res.status(200).send(response.data); 
+    }catch(error){
+      console.log(error);
+
+      res.status(401).json({message:error});
+
+    }
+    
+    
 
 
 })
--- src/components/Payment/index.jsx
+++ src/components/Payment/index.jsx
@@ -15,41 +15,38 @@ function Payment() {
     const stripe = useStripe();
     const elements = useElements();
 
+    const cashfree = new window.Cashfree({
+        mode:"sandbox" //or production
+    });
+
     const [succeeded, setSucceeded] = useState(false);
     const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
-    const [disabled, setDisabled] = useState(true);
+    const [disabled, setDisabled] = useState(false);
     const [clientSecret, setClientSecret] = useState(true);
 
-    useEffect(() => {
-        // generate the special stripe secret which allows us to charge a customer
-       console.log("hrllo")
-    }, [basket])
-    useEffect(() => {
-        // generate the special stripe secret which allows us to charge a customer
-        const getClientSecret = async () => {
-            const response = await axios({
-                method: 'post',
-                // Stripe expects the total in a currencies subunits
-                url: `/payments/create?total=${getBasketTotal(basket) * 100}`
-            });
-            console.log("Hello",response.data)
-            setClientSecret(response.data.clientSecret)
-        }
 
-        getClientSecret();
-    }, [basket])
+    // useEffect(() => {
+    //     // generate the special stripe secret which allows us to charge a customer
+    //     const getClientSecret = async () => {
+    //         const response = await axios({
+    //             method: 'post',
+    //             // Stripe expects the total in a currencies subunits
+    //             url: `/payments/create?total=${getBasketTotal(basket) * 100}`
+    //         });
+    //         console.log("Hello",response.data)
+    //         setClientSecret(response.data.clientSecret)
+    //     }
+
+    //     getClientSecret();
+    // }, [basket])
 
     const handleSubmit = async (event) => {
-        // do all the fancy stripe stuff...
         event.preventDefault();
-        setProcessing(true);
-        console.log("Tdd")
-        const amount = basket?.reduce((acc,item)=> acc+item.price,0);
+        // setProcessing(true);
 
-        console.log("thisisuser",amount);
         const body = JSON.stringify({
-            amount:amount,
+            amount:getBasketTotal(basket),
             customerId:user.email,
             customerPhone:"9871887737"
         })
@@ -60,7 +57,7 @@ function Payment() {
                 "Content-Type": "application/json"
             },
             data: {
-                amount:amount,
+                amount:getBasketTotal(basket),
                 customerId:user.email,
                 customerPhone:"9871887737"
             }
@@ -69,10 +66,79 @@ function Payment() {
         console.log(response);
 
         const payment_session_id = response?.data?.payment_session_id;
-        console.log(window.Cashfree);
-        const cashfree = new window.Cashfree(payment_session_id);
-        cashfree.redirect();
+        const orderid=response?.data?.order_id;
+        // console.log(window.Cashfree);
+        // const cashfree = new window.Cashfree(payment_session_id);
+        // cashfree.redirect();
 
+        let checkoutOptions = {
+            paymentSessionId: payment_session_id,
+            // returnUrl: `http://localhost:5173/orders?order_id=${order_id}`,
+            
+        }
+        cashfree.checkout(checkoutOptions).then(function(result){
+
+            db
+              .collection('users')
+              .doc(user?.uid)
+              .collection('orders')
+              .doc(orderid.id)
+              .set({
+                  basket: basket,
+                  amount: orderid.amount,
+                  created: orderid.created
+              })
+              for (let i = 1; i <= 1000; i++) {
+                console.log(i);
+            }
+            
+
+            if(result.error){
+                alert(result.error.message)
+            }
+            else{
+                console.log("Redirection")
+                result.redirect
+            }
+        });
+
+
+
+        // const dropinConfig = 
+        // {
+        //     paymentSessionId:payment_session_id,
+        //     components: 
+        //     [
+        //         "order-details",
+        //         "card",
+        //         "netbanking",
+        //         "app",
+        //         "upi",
+        //     ],
+        //     onSuccess: function(data){
+        //        //on success
+        //     },
+        //     onFailure: function(data){
+        //        //on success
+        //     },
+        //     style: 
+        //     {
+        //         //to be replaced by the desired values
+        //         backgroundColor: "#ffffff",
+        //         color: "#11385b", 
+        //         fontFamily: "Lato",
+        //         fontSize: "14px",
+        //         errorColor: "#ff0000",
+        //         theme: "light"
+        //     }
+        // }
+        // console.log("B4 drop");
+        // const element = document.getElementsByClassName('payment')[0]
+        // console.log(element);
+        
+        // cashfree.drop(element,dropinConfig);
+        
+        // console.log("aftr drop");
     }
 
     const handleChange = event => {
